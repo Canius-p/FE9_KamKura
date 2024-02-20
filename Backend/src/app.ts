527ed9c3988a61e65c83cf60ebe7dd3b40745dcc
Canius <canius@pop-os.localdomain>
2024-02-20 23:38:21 +0545

    routes setups

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -2,20 +2,30 @@ import express from "express";
 import * as dotenv from "dotenv";
 import cors from "cors";
 import path from "path";
+import fileUpload from "express-fileupload";
+import cookieParser from "cookie-parser";
 
 const app = express();
 dotenv.config({ path: "../../Backend/" });
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: ["process.env.FRONTEND_URL"],
     methods: ["GET", "POST", "PUT", "PATCH"],
     credentials: true,
   })
 );
 
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp/",
+  })
+);
 // Example route
 app.get("/", (req, res) => {
   res.send("Hello, World!");
